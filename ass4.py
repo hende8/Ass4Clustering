582f91a8ee328b2c8efd3894f272209522200845
shirang <shirangolzar@gmail.com>
2020-06-14 17:16:59 +0300

    task 1

--- ass4.py
+++ ass4.py
@@ -1,24 +1,32 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-data = pd.read_excel("C:\\Users\\HEN\\Downloads\\Data.xlsx",header=0)
-data.to_csv("C:\\Users\\HEN\\Downloads\\Data.csv", index=False, quotechar="'")
-df = pd.read_csv("C:\\Users\\HEN\\Downloads\\Data.csv")
-# print(df)
-def clearna(data):
-    for col in list(data):
+def preProcessingData(link):
+    data = pd.read_excel(link,header=0)
+    urlArray= link.split("\\")
+    urlArray.pop(len(urlArray)-1)
+    url=""
+    for partLink in urlArray:
+        url+=partLink+"\\"
+    data.to_csv(url+"Data.csv", index=False, quotechar="'")
+    df = pd.read_csv(url+"Data.csv")
+
+    #completing missing values in mean
+    for col in list(df):
         if(col=='country'):continue
         data[col].fillna(data[col].mean(), inplace=True)
-    return data
 
-df = clearna(df)
-standartscalar= StandardScaler()
-df_scaled = pd.DataFrame(standartscalar.fit_transform(df.iloc[:,2:]),columns = df.columns[2:])
-new = df[['country', 'year']].copy()
-finalStandard = new.join(df_scaled)
-df3 = finalStandard.groupby('country').mean()
+    #normalized by Standardization
+    standartscalar= StandardScaler()
+    df_scaled = pd.DataFrame(standartscalar.fit_transform(df.iloc[:,2:]),columns = df.columns[2:])
 
-#
-# df4 = pd.concat([df.columns[:1], df_scaled.reset_index(drop=True)], axis=1)
-# print(df4)
+    #join columns 'country' and 'year' to one dataframe
+    new = df[['country', 'year']].copy()
+    finalStandard = new.join(df_scaled)
 
+    #group by country
+    finalDf = finalStandard.groupby('country').mean()
+
+    return finalDf
+
+print(preProcessingData("C:\\Users\\shira\\Desktop\\shiran\\Dataset.xlsx"))
