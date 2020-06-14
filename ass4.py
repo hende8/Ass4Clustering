3e000d963ea2e4498db8741e1a6f4033cfb013ed
shirang <shirangolzar@gmail.com>
2020-06-14 15:12:04 +0300

    test

--- ass4.py
+++ ass4.py
@@ -1,15 +1,20 @@
 import pandas as pd
 import xlrd
 
-data = pd.read_excel("C:\\Users\\HEN\\Downloads\\Data.xlsx",header=0)
-data.to_csv("C:\\Users\\HEN\\Downloads\\Data.csv", index=False, quotechar="'")
-df = pd.read_csv("C:\\Users\\HEN\\Downloads\\Data.csv")
+data = pd.read_excel("C:\\Users\\shira\\Desktop\\shiran\\Dataset.xlsx", header=0)
+data.to_csv("C:\\Users\\shira\\Desktop\\shiran\\Data.cvc", index=False, quotechar="'")
+df = pd.read_csv("C:\\Users\\shira\\Desktop\\shiran\\Data.cvc")
 # print(df)
 for col in list(df):
     if(col=='country'):continue
     df[col].fillna(df[col].mean(), inplace=True)
 
 print(df.head(10))
+
+df3 = df.groupby('country').mean()
+print(df3.head(10))
+for col in df3:
+    print(df3[col].head(10))
 
 # c = d6tstack.convert_xls.XLStoCSVMultiSheet('C:\\Users\\HEN\\Downloads\\Data.xlsx')
 # c.convert_all() # ['multisheet-Sheet1.csv','multisheet-Sheet2.csv']
